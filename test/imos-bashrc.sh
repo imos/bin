f08f4ccd937fe046582fd98f0f068d4635b9fb8f
imos <git@imoz.jp>
2014-10-31 03:31:32 +0900

    Add a test for every operating system.

---
+++ test/imos-bashrc.sh
@@ -0,0 +1,2 @@
+source "$(dirname "${BASH_SOURCE}")"/../imos-bashrc || exit 1
+eval "echo \"${PS1}\"" > '/dev/null'
--- test/imos-bashrc_test.sh
+++ test/imos-bashrc_test.sh
@@ -1,6 +1,8 @@
 test::imos-bashrc() {
-  PS1='$'
-  POKEMON='Pikachu'
-  source "$(dirname "${BASH_SOURCE}")/../imos-bashrc"
-  eval "echo \"${PS1}\"" > '/dev/null'
+  PS1='$' POKEMON='Pikachu' UNAME='Darwin' \
+      "$(dirname "${BASH_SOURCE}")"/imos-bashrc.sh
+  PS1='$' POKEMON='Pikachu' UNAME='Linux' \
+      "$(dirname "${BASH_SOURCE}")"/imos-bashrc.sh
+  PS1='$' POKEMON='Pikachu' UNAME='Unknown' \
+      "$(dirname "${BASH_SOURCE}")"/imos-bashrc.sh
 }
