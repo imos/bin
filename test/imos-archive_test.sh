e19404ba5f882953f001b955f9b7815e9dfcb2ae
imos <git@imoz.jp>
2014-12-14 12:30:36 +0900

    Add imos-archive.

---
+++ test/imos-archive_test.sh
@@ -0,0 +1,14 @@
+run() {
+  local output_file=''
+  func::tmpfile output_file
+  ../imos-archive --output="${output_file}" "$@"
+  "${output_file}"
+}
+
+test::imos-archive() {
+  pushd "$(dirname "${BASH_SOURCE}")"
+  EXPECT_EQ 'foo bar' "$(run --command='echo foo bar')"
+  EXPECT_EQ 'foo bar' "$(run --command='echo foo bar' --file_size=1000000)"
+  EXPECT_DEATH "$(run --command='echo foo bar' --file_size=1000)"
+  EXPECT_EQ 'This is imos-archive.' "$(run --command='./imos-archive.sh')"
+}
