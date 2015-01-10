8cda5bf3038e6202d35227f5aeb6de185d7abb6e
imos <git@imoz.jp>
2015-01-11 02:03:44 +0900

    Fix imos-archive_test.

--- test/imos-archive_test.sh
+++ test/imos-archive_test.sh
@@ -2,22 +2,20 @@ run() {
   local tmporary_directory=''
   func::tmpfile temporary_directory
   mkdir -p "${temporary_directory}"
-  # pushd "${temporary_directory}" >'/dev/null'
   local output_file=''
   func::tmpfile output_file
-  imos-archive --output="${output_file}" "$@"
+  "$(dirname "${BASH_SOURCE}")/../imos-archive" \
+      --output="${output_file}" "$@"
   if [ "${#run_options[*]}" -ne 0 ]; then
     "${output_file}" "${run_options[@]}"
   else
     "${output_file}"
   fi
-  # popd >'/dev/null'
 }
 
 test::imos-archive() {
   local run_options=()
   local sample_command="$(dirname "${BASH_SOURCE}")/imos-archive.sh"
-  # pushd "$(dirname "${BASH_SOURCE}")"
   EXPECT_EQ 'foo bar' "$(run --command='echo foo bar')"
   EXPECT_EQ 'foo bar' "$(run --command='echo foo bar' --file_size=1000000)"
   EXPECT_DEATH "$(run --command='echo foo bar' --file_size=1000)"
