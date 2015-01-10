0590858fd47875b1ece9e2645461d1ac7a421702
imos <git@imoz.jp>
2015-01-11 01:36:11 +0900

    Fix imos-archive_test.

--- test/imos-archive_test.sh
+++ test/imos-archive_test.sh
@@ -1,28 +1,31 @@
 run() {
   local tmporary_directory=''
   func::tmpfile temporary_directory
-  mkdir -p "${tmporary_directory}"
-  cd "${temporary_directory}"
+  mkdir -p "${temporary_directory}"
+  # pushd "${temporary_directory}" >'/dev/null'
   local output_file=''
   func::tmpfile output_file
-  ../imos-archive --output="${output_file}" "$@"
+  imos-archive --output="${output_file}" "$@"
   if [ "${#run_options[*]}" -ne 0 ]; then
     "${output_file}" "${run_options[@]}"
   else
     "${output_file}"
   fi
+  # popd >'/dev/null'
 }
 
 test::imos-archive() {
   local run_options=()
-  pushd "$(dirname "${BASH_SOURCE}")"
+  local sample_command="$(dirname "${BASH_SOURCE}")/imos-archive.sh"
+  # pushd "$(dirname "${BASH_SOURCE}")"
   EXPECT_EQ 'foo bar' "$(run --command='echo foo bar')"
   EXPECT_EQ 'foo bar' "$(run --command='echo foo bar' --file_size=1000000)"
   EXPECT_DEATH "$(run --command='echo foo bar' --file_size=1000)"
-  EXPECT_EQ '# of args: 0' "$(run --command='./imos-archive.sh')"
+  EXPECT_EQ '# of args: 0' "$(run --command="${sample_command}")"
   run_options=('foo bar')
-  EXPECT_EQ $'# of args: 1\nfoo bar' "$(run --command='./imos-archive.sh')"
+  EXPECT_EQ $'# of args: 1\nfoo bar' "$(run --command="${sample_command}")"
   run_options=('foo' 'bar')
-  EXPECT_EQ $'# of args: 2\nfoo\nbar' "$(run --command='./imos-archive.sh')"
-  EXPECT_EQ $'# of args: 0' "$(run --command='./imos-archive.sh' --noextra_arguments)"
+  EXPECT_EQ $'# of args: 2\nfoo\nbar' "$(run --command="${sample_command}")"
+  EXPECT_EQ $'# of args: 0' "$(
+      run --command="${sample_command}" --noextra_arguments)"
 }
